30466861315c7c31e32323d3973a8e2e2607cf68
Shota Jolbordi <shota.jolbordi@protonmail.com>
2020-12-14 02:51:51 +0400

    Update Dependencies structure

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -1,12 +1,29 @@
 import sbt._
 
 object Dependencies {
-  val test = Seq(
-    "org.scalatest"     %% "scalatest"       % "3.2.3",
-    "org.scalacheck"    %% "scalacheck"      % "1.15.1",
-    "org.scalatestplus" %% "scalacheck-1-14" % "3.2.2.0"
-  )
-  val core = Seq(
-    "com.lihaoyi" %% "os-lib" % "0.7.1"
-  )
+
+  object Test {
+    object org {
+      object scalatest {
+        val scalatest = "org.scalatest" %% "scalatest" % "3.2.3"
+      }
+
+      object scalacheck {
+        val scalacheck = "org.scalacheck" %% "scalacheck" % "1.15.1",
+      }
+
+      object scalatestplus {
+        val `scalacheck-1-14` =
+          "org.scalatestplus" %% "scalacheck-1-14" % "3.2.2.0"
+      }
+    }
+  }
+
+  object Core {
+    object com {
+      object lihaoyi {
+        val `os-lib` = "com.lihaoyi" %% "os-lib" % "0.7.1"
+      }
+    }
+  }
 }
